e04c44bf928ee4d004db37be6dd488e707b8bf09
haroon-ignitehq <hmahmood@ignitehq.io>
2022-08-25 07:13:41 +0500

    Axios Interceptors configured

---
+++ src/helpers/axiosInterceptior.js
@@ -0,0 +1,24 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import env from '../config/env';
+
+const headers = {}
+const instance = axios.create({
+    baseURL: env.BACKEND_URL,
+    headers,
+});
+
+instance.interceptors.request.use(
+    async config => {
+        const token = await AsyncStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+
+    }, error => {
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
